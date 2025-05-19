f918f321799ae3d5e7ef04c00a096c4126474de1
TDMedina <tylerdanmedina@gmail.com>
2025-05-19 17:38:47 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="cmd_fusion",
+    version="0.1.0",
+    description="Fusion results comparison dashboard.",
+    url="https://github.com/TDMedina/CMD_Fusion",
+    author="T.D. Medina",
+    author_email="tylerdanmedina@gmail.com",
+    packages=["cmd_fusion"],
+    zip_safe=False,
+    python_requires=">=3.10",
+    install_requires=[
+        "dash==2.18.2",
+        "dash-bootstrap-components==1.7.1",
+        "liftover==1.3.2",
+        "numpy==2.2.3",
+        "pandas==2.2.3",
+        "plotly==6.0.0",
+        "pysam==0.23.0",
+        "PyYAML==6.0.2",
+        "tqdm==4.67.1"
+        ],
+    entry_points={
+        "console_scripts": [
+            "FusionDash = cmd_fusion.dashboard.__main__",
+            ]
+        }
+    )
