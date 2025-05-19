de7743b54a3363d27562309592b64606373bf6ae
TDMedina <tylerdanmedina@gmail.com>
2025-05-19 22:47:54 +0100

    Removed init contents.

--- cmd_fusion/__init__.py
+++ cmd_fusion/__init__.py
@@ -1,2 +1,2 @@
-# from . import parsers
+from . import parsers
 # from . import data_objects
--- cmd_fusion/parsers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .arriba import ArribaFusions
-from .starfusion import StarFusions
-from .thermo_ir import ThermoFusions
-from .cmd_vcf import VCFFusions
