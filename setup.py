845473b6c8d953169293abc5006d662a336c6624
TDMedina <tylerdanmedina@gmail.com>
2025-05-19 23:54:10 +0100

    Upgraded python version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email="tylerdanmedina@gmail.com",
     packages=find_packages(),
     zip_safe=False,
-    python_requires=">=3.10",
+    python_requires=">=3.12",
     install_requires=[
         "dash==2.18.2",
         "dash-bootstrap-components==1.7.1",
